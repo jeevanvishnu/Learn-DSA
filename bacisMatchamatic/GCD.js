2b02c543f9cf44276766653a50bf0dd44c563b59
Jeevan Vishnu <140521914+jeevanvishnu@users.noreply.github.com>
2025-10-02 23:34:52 +0530

    Add GCD calculation

--- bacisMatchamatic/GCD.js
+++ bacisMatchamatic/GCD.js
@@ -1,3 +1,27 @@
-function gcd (a , b) {
-    
+// function gcd (a , b) {
+//     let res =  Math.min(a , b)
+
+//     while(res > 0){
+//         if(a % res === 0 && b % res === 0){
+//             return res
+//         }else {
+//             res --
+//         }
+//     }
+//     return res
+// }
+
+// console.log(gcd(6 , 3));
+
+
+
+// simple method 
+
+function gcd (a , b){
+    if(b === 0){
+        return a
+    }
+    return (b , a % b)
 }
+
+console.log(gcd(15 , 10));
